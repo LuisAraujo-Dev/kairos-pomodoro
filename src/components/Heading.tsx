6124d9788ab418a295f34691e2ef9d6cb7397484
Luís Araújo <luisaraujo.dev@gmail.com>
2025-05-22 14:39:55 -0300

    refactore: altered HeadingPromps type for Reac.ReactNode

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,7 +1,8 @@
+import type React from 'react';
 import styles from './Heading.module.css';
 
 type HeadingProps = {
-    children: string;   
+    children: React.ReactNode;   
 }; 
 
 export function Heading({ children }: HeadingProps) { 
