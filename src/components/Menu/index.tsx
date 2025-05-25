688339e38fac853c9d1f2770e3dea32a0637cecb
Luís Araújo <luisaraujo.dev@gmail.com>
2025-05-25 03:38:34 -0300

    feat: add aria-label and title in nav menu

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -4,19 +4,19 @@ import styles from "./styles.module.css";
 export function Menu() {
   return (
     <nav className={styles.menu}>
-      <a href="#" className={styles.menuLink}>
+      <a href="#" className={styles.menuLink} aria-label="Ir para Home" title="Ir para Home">
         <HouseIcon />
       </a>
 
-      <a href="#" className={styles.menuLink}>
+      <a href="#" className={styles.menuLink} aria-label="Ver hitórico" title="Ver hitórico">
         <HistoryIcon />
       </a>
 
-      <a href="#" className={styles.menuLink}>
+      <a href="#" className={styles.menuLink} aria-label="Configurações" title="Configurações">
         <SettingsIcon />
       </a>
 
-      <a href="#" className={styles.menuLink}>
+      <a href="#" className={styles.menuLink} aria-label="Mudar de tema" title="Mudar de tema">
         <SunIcon />
       </a>
     </nav>
