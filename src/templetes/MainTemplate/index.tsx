51c78c39b0ce679b3f632ed39002a7cef635c861
Luís Araújo <luisaraujo.dev@gmail.com>
2025-05-25 15:38:17 -0300

    reat: Create MainTemplatePros and children in MainTemplete

---
+++ src/templetes/MainTemplate/index.tsx
@@ -0,0 +1,30 @@
+import "./styles/theme.css";
+import "./styles/global.css";
+import { Container } from "../../components/Container";
+import { Logo } from "../../components/Logo";
+import { Menu } from "../../components/Menu";
+import { Footer } from "../../components/Footer/Index";
+
+type MainTemplatesPros = {
+    children :React.ReactNode;
+}
+
+export function MainTemplate({ children }: MainTemplatesPros) {
+  return (
+    <>
+      <Container>
+        <Logo />
+      </Container>
+
+      <Container>
+        <Menu />
+      </Container>
+
+      {children}
+
+      <Container>
+        <Footer />
+      </Container>
+    </>
+  );
+}
