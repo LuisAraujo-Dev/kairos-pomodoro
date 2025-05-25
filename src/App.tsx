8fe75a2866de591babe526afe88b3cab7e2a560f
Luís Araújo <luisaraujo.dev@gmail.com>
2025-05-25 15:22:32 -0300

    refactore: creat MainForm component to contain the form

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,8 @@ import { Container } from "./components/Container";
 import { Logo } from "./components/Logo";
 import { Menu } from "./components/Menu";
 import { CountDown } from "./components/CountDown";
-import { DeufaltInput } from "./components/DefaultInput";
-import { Cycles } from "./components/Cycles";
-import { DefaultButton } from "./components/DefaultButton";
-import { CircleStopIcon, PlayCircleIcon } from "lucide-react";
 import { Footer } from "./components/Footer/Index";
+import { MainForm } from './components/MainForm'
 
 export function App() {
   return (
@@ -27,28 +24,7 @@ export function App() {
       </Container>
 
       <Container>
-        <form className="form" action="">
-          <div className="formRow">
-            <DeufaltInput 
-            labelText="task:" 
-            id="meuImput" 
-            type="text" 
-            placeholder="Digite algo"/>
-          </div>
-
-          <div className="formRow">
-            <p>Lorem ipsum dolor sit amet.</p>
-          </div>
-
-          <div className="formRow">
-            <Cycles />
-          </div>
-
-          <div className="formRow">
-            <DefaultButton icon={<PlayCircleIcon/>} color="green"/> 
-            <DefaultButton icon={<CircleStopIcon/>} color="red"/> 
-          </div>
-        </form>
+        <MainForm />
       </Container>
 
       <Container>
--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,37 +1,48 @@
-import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from "lucide-react";
-import styles from "./styles.module.css";
-import { useEffect, useState } from "react";
+import {
+  HistoryIcon,
+  HouseIcon,
+  MoonIcon,
+  SettingsIcon,
+  SunIcon,
+} from 'lucide-react'; 
+import styles from './styles.module.css';
+import { useState, useEffect } from 'react';
 
-type AvailableThemes = "dark" | "light";
+type AvailableThemes = 'dark' | 'light';
 
 export function Menu() {
   const [theme, setTheme] = useState<AvailableThemes>(() => {
-    const storageTheme = (
-      localStorage.setIten('theme') as AvailableThemes) || 'dark';
-      return storageTheme;
+    const storageTheme =
+      (localStorage.getItem('theme') as AvailableThemes) || 'dark';
+    return storageTheme;
   });
 
+  const nextThemeIcon = {
+    dark: <SunIcon/>,
+    light: <MoonIcon/>
+  }
+
   function handleThemeChange(
-    event: React.MouseEvent<HTMLAnchorElement, MouseEvent>
+    event: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
   ) {
     event.preventDefault();
 
     setTheme((prevTheme) => {
       const nextTheme = prevTheme === "dark" ? "light" : "dark";
-      localStorage.setItem('theme', theme); 
       return nextTheme;
     });
   }
 
   useEffect(() => {
-    document.documentElement.setAttribute("data-theme", theme);
+    document.documentElement.setAttribute('data-theme' , theme);
+    localStorage.setItem('theme', theme);
   }, [theme]);
 
   return (
     <nav className={styles.menu}>
       <a
+      className={styles.menuLink}
         href="#"
-        className={styles.menuLink}
         aria-label="Ir para Home"
         title="Ir para Home"
       >
@@ -63,7 +74,7 @@ export function Menu() {
         title="Mudar de tema"
         onClick={handleThemeChange}
       >
-        <SunIcon />
+        {nextThemeIcon[theme]}
       </a>
     </nav>
   );
