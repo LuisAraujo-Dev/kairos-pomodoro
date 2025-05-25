35e46f04ae938d9dc2030dfbcb4c81461003d5dd
Luís Araújo <luisaraujo.dev@gmail.com>
2025-05-25 02:36:27 -0300

    feat: creat footer component

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import "./styles/theme.css";
 import "./styles/global.css";
 
 import { Container } from "./components/Container";
-import { Heading } from "./components/Heading/Index";
 import { Logo } from "./components/Logo";
 import { Menu } from "./components/Menu";
 import { CountDown } from "./components/CountDown";
@@ -10,6 +9,7 @@ import { DeufaltInput } from "./components/DefaultInput";
 import { Cycles } from "./components/Cycles";
 import { DefaultButton } from "./components/DefaultButton";
 import { CircleStopIcon, PlayCircleIcon } from "lucide-react";
+import { Footer } from "./components/Footer/Index";
 
 export function App() {
   return (
@@ -52,7 +52,7 @@ export function App() {
       </Container>
 
       <Container>
-        <Heading>Título</Heading>
+        <Footer />
       </Container>
     </>
   );
---
+++ src/components/Footer/Index.tsx
@@ -0,0 +1,11 @@
+import styles from "./styles.module.css";
+
+
+export function Footer() {
+  return (
+      <footer className={styles.footer}>
+        <a href="#">Entenda como funciona a técnica Pomodoro</a>
+        <a href="#">Kairos Pomodoro &copy; Feito por LuÍS Araújo</a>
+      </footer>
+  )
+}
