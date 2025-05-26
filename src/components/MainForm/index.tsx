c3b505238bf5993114d0a3e8e7d0eb0fcecb0caf
Luís Araújo <luisaraujo.dev@gmail.com>
2025-05-26 18:47:27 -0300

    Refactore, remove Defalut Button Red

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -1,31 +1,31 @@
-import { CircleStopIcon, PlayCircleIcon } from "lucide-react";
+import { PlayCircleIcon } from "lucide-react";
 import { DefaultButton } from "../DefaultButton";
 import { DeufaltInput } from "../DefaultInput";
 import { Cycles } from "../Cycles";
 
 export function MainForm() {
+  return (
+    <form className="form" action="">
+      <div className="formRow">
+        <DeufaltInput
+          labelText="task:"
+          id="meuImput"
+          type="text"
+          placeholder="Digite algo"
+        />
+      </div>
 
-return (<form className="form" action="">
-          <div className="formRow">
-            <DeufaltInput 
-            labelText="task:" 
-            id="meuImput" 
-            type="text" 
-            placeholder="Digite algo"/>
-          </div>
+      <div className="formRow">
+        <p>Lorem ipsum dolor sit amet.</p>
+      </div>
 
-          <div className="formRow">
-            <p>Lorem ipsum dolor sit amet.</p>
-          </div>
+      <div className="formRow">
+        <Cycles />
+      </div>
 
-          <div className="formRow">
-            <Cycles />
-          </div>
-
-          <div className="formRow">
-            <DefaultButton icon={<PlayCircleIcon/>} color="green"/> 
-            <DefaultButton icon={<CircleStopIcon/>} color="red"/> 
-          </div>
-        </form>)
-
-        };
+      <div className="formRow">
+        <DefaultButton icon={<PlayCircleIcon />} color="green" />
+      </div>
+    </form>
+  );
+}
--- src/templetes/MainTemplate/index.tsx
+++ src/templetes/MainTemplate/index.tsx
@@ -1,5 +1,4 @@
-import "./styles/theme.css";
-import "./styles/global.css";
+
 import { Container } from "../../components/Container";
 import { Logo } from "../../components/Logo";
 import { Menu } from "../../components/Menu";
