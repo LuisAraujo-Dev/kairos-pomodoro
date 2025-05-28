3fe2b301aeff10eb86eaac52a407c2828e49859f
Luís Araújo <luisaraujo.dev@gmail.com>
2025-05-28 00:20:31 -0300

    refactore: transferred initialState const to TaskContext

--- src/contexts/TaskContext/index.tsx
+++ src/contexts/TaskContext/index.tsx
@@ -1,6 +1,19 @@
 import { createContext } from "react";
 import type { TaskStateModels } from "../../models/TaskStateModels";
 
+const initialState: TaskStateModels = {
+  tasks: [],
+  secondsRemainig: 0, 
+  formattedSecondsRemaining: '00:00', 
+  activeTask: null, 
+  currentCycle: 0, 
+  config: {
+    workTime: 25, 
+    shortBreakTime: 5, 
+    longBreakTime: 15,
+  }, 
+  };
+
 export const TaskContext = createContext({
     chave: '123',
 });
