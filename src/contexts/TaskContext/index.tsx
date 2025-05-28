a821f25f2eccdf73f065186ae3b072feb00a9368
Luís Araújo <luisaraujo.dev@gmail.com>
2025-05-28 00:11:41 -0300

    feat

--- src/contexts/TaskContext/index.tsx
+++ src/contexts/TaskContext/index.tsx
@@ -1,5 +1,16 @@
 import { createContext } from "react";
+import type { TaskStateModels } from "../../models/TaskStateModels";
 
 export const TaskContext = createContext({
     chave: '123',
 });
+
+type TaskContextProps = {
+    state: TaskStateModels, 
+    setState: React.Dispatch<React.SetStateAction<TaskStateModels>>
+};
+
+export const taskContext = createContext<TaskContextProps>({
+    state: initialState, 
+    setState: () => {}
+})
