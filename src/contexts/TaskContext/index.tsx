05d975c97590272b1efb02da480b9854f3eb4abc
Luís Araújo <luisaraujo.dev@gmail.com>
2025-05-28 00:36:24 -0300

    feat: create TaskContextProviderProps type

--- src/contexts/TaskContext/index.tsx
+++ src/contexts/TaskContext/index.tsx
@@ -1,29 +1,29 @@
-import { createContext } from "react";
+import React, { createContext } from "react";
 import type { TaskStateModels } from "../../models/TaskStateModels";
 
 const initialState: TaskStateModels = {
   tasks: [],
-  secondsRemainig: 0, 
-  formattedSecondsRemaining: '00:00', 
-  activeTask: null, 
-  currentCycle: 0, 
+  secondsRemainig: 0,
+  formattedSecondsRemaining: "00:00",
+  activeTask: null,
+  currentCycle: 0,
   config: {
-    workTime: 25, 
-    shortBreakTime: 5, 
+    workTime: 25,
+    shortBreakTime: 5,
     longBreakTime: 15,
-  }, 
-  };
-
-export const TaskContext = createContext({
-    chave: '123',
-});
+  },
+};
 
 type TaskContextProps = {
-    state: TaskStateModels, 
-    setState: React.Dispatch<React.SetStateAction<TaskStateModels>>
+  state: TaskStateModels;
+  setState: React.Dispatch<React.SetStateAction<TaskStateModels>>;
 };
 
 export const taskContext = createContext<TaskContextProps>({
-    state: initialState, 
-    setState: () => {}
-})
+  state: initialState,
+  setState: () => {},
+});
+
+type TaskContextProviderProps = {
+    children: React.ReactNode; 
+}
