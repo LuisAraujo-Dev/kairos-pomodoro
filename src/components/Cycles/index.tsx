b36e2dfffec25e4408b461e92f09eac59b6e9bd0
Luís Araújo <luisaraujo.dev@gmail.com>
2025-05-31 14:32:24 -0300

    feat: create cycleStep const to Cycles

--- src/components/Cycles/index.tsx
+++ src/components/Cycles/index.tsx
@@ -1,6 +1,13 @@
 import styles from "./styles.module.css";
+import { useTaskContext } from "../../contexts/TaskContext/UseTaskContext";
 
 export function Cycles() {
+  const {state} = useTaskContext(); 
+
+  const cycleStep = Array.from({ length: state.currentCycle})
+
+  console.log(cycleStep); 
+
   return (
     <div className={styles.cycles}>
       <span>Ciclos</span>
