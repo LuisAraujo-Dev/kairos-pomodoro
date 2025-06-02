e09f9f5f262425f85757a6231100d61f3f6d4125
Luís Araújo <luisaraujo.dev@gmail.com>
2025-06-02 20:36:06 -0300

    feat: create tipsForWhenActiveTask and tipsForNoActiveTask variables in Tips

--- src/Tips/index.tsx
+++ src/Tips/index.tsx
@@ -1,3 +1,18 @@
 export function Tips() {
-    
+  <>
+    {!state.activeTask && tipsForWhenActiveTask[state.activeTask.type]}
+    {!!state.activeTask && tipsForNoActiveTask[nextCycleType]}
+  </>;
 }
+
+const tipsForWhenActiveTask = {
+  workTime: <span>Foque por {state.config.workTime}min</span>,
+  shortBreakTime: <span>Descanse por {state.config.shortBreakTime}</span>,
+  longBreakTime: <span>Descanse por {state.config.longBreakTime}</span>,
+};
+
+const tipsForNoActiveTask = {
+  workTime: <span>Próximo ciclo é de {state.config.workTime}min</span>,
+  shortBreakTime: <span>Próximo descando será de {state.config.shortBreakTime}</span>,
+  longBreakTime: <span>Próximo descando será de {state.config.longBreakTime}</span>,
+};
