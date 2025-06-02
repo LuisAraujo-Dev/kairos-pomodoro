f666303cc60a853a1cb7850ed150fc63926f92a8
Luís Araújo <luisaraujo.dev@gmail.com>
2025-06-02 01:14:13 -0300

    feat: create TaskActiveModels type in taskActions file

--- src/contexts/TaskContext/taskActions.ts
+++ src/contexts/TaskContext/taskActions.ts
@@ -1,4 +1,11 @@
+import type { TaskModels } from "../../models/TaskModels";
+
 export enum TaskActionTypes {
-    START_TASK = 'START_TASK',
-    INTERRUPT_TASK = 'INTERRUPT_TASK',
+  START_TASK = "START_TASK",
+  INTERRUPT_TASK = "INTERRUPT_TASK",
 }
+
+export type TaskActionModels = {
+  type: TaskActionTypes.START_TASK;
+  payload: TaskModels;
+};
