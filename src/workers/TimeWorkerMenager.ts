22a2d11be4b6ea7a2bcb5c895b7bb602be9b476a
Luís Araújo <luisaraujo.dev@gmail.com>
2025-06-03 12:47:00 -0300

    feat: creat instance class and constructor and static metod on TimeWorkerManeger

--- src/workers/TimeWorkerMenager.ts
+++ src/workers/TimeWorkerMenager.ts
@@ -1,3 +1,17 @@
-export class TimeWorkerManeget {
-    
+let instance: TimeWorkerManeger | null = null; 
+
+export class TimeWorkerManeger {
+    private worker: Worker; 
+
+    private constructor () {
+        this.worker = new Worker( new URL('./timerWorker.js', import.meta.url))
+    }
+
+    static getIntance() {
+        if (!instance) {
+            instance = new TimeWorkerManeger(); 
+        }
+
+        return instance; 
+    }
 }
