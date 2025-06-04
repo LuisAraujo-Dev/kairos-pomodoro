40fd82beaf6d070e59caa4e5c11e93b7b0d82d0a
Luís Araújo <luisaraujo.dev@gmail.com>
2025-06-04 14:07:43 -0300

    feat: add theme to toastify

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,27 @@ import "./styles/theme.css";
 import "./styles/global.css";
 import { TaskContextProvider } from "./contexts/TaskContext/TaskContextProvider";
 import { Home } from "./pages/Home";
+import { ToastContainer } from "react-toastify/unstyled";
+
 
 export function App() {
   return (
     <TaskContextProvider>
       <Home />
+
+      <ToastContainer
+position="top-center"
+autoClose={10000}
+hideProgressBar={false}
+newestOnTop={false}
+closeOnClick={true}
+rtl={false}
+pauseOnFocusLoss
+draggable
+pauseOnHover
+theme="light"
+transition={Bounce}
+/>
     </TaskContextProvider>
   );
 }
