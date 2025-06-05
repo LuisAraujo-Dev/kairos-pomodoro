0bfd704bdd9d92e5aefa00625cb770e4f97decf7
Luís Araújo <luisaraujo.dev@gmail.com>
2025-06-04 22:02:51 -0300

    feat: create ScrollToTop function to scrow top page

--- src/routers/MainRouter/index.tsx
+++ src/routers/MainRouter/index.tsx
@@ -1,19 +1,29 @@
+import { useEffect } from 'react';
 import { AboutPomodoro } from '../../pages/AboutPomodoro';
 import { Home } from '../../pages/Home';
 import { NotFound } from '../../pages/NotFould';
-import { BrowserRouter, Route, Routes } from 'react-router';
+import { BrowserRouter, Route, Routes, useLocation } from 'react-router';
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({ top: 0 });
+  }, [pathname]);
+
+  return null;
+}
 
 export function MainRouter() {
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/about-pomodoro/' element={<AboutPomodoro />} />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about-pomodoro/' element={<AboutPomodoro />} />
 
-          <Route path='*' element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </>
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+      <ScrollToTop></ScrollToTop>
+    </BrowserRouter>
   );
 }
