312b72aaf37e80ec6ce28e91408e850b8b40be2d
Luís Araújo <luisaraujo.dev@gmail.com>
2025-06-07 20:32:22 -0300

    refactore: change title page history

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -81,7 +81,7 @@ export function History() {
     <MainTemplate>
       <Container>
         <Heading>
-          <span>History</span>
+          <span>Histórico</span>
 
           {hasTasks && (
             <span className={styles.buttonContainer}>
